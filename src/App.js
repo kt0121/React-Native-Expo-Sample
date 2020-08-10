140b4cc46e2581ac68003f7f0b72778e6dd9ff63
Narikawa <38754367+TakaoNarikawa@users.noreply.github.com>
2020-08-10 18:09:35 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from "react";
+import { Button } from "react-native";
 import styled from "styled-components/native";
 import { css } from "styled-components";
 
+const RoundButton = (props) => {
+  return (
+    <RoundButtonWrapper>
+      <Button title={props.title} color="white" onPress={props.onPress} />
+    </RoundButtonWrapper>
+  );
+};
+
+const Card = (props) => {
+  return (
+    <CardContainer>
+      <Text style={{ padding: 14 }}>タイトル：{props.title}</Text>
+      <RoundButton title="Click me !!" onPress={() => console.log("Simple Button pressed")} />
+    </CardContainer>
+  );
+};
+
 const App = () => {
-  const title = "fooo";
-  const onClickButton = () => {
-    console.log("foooo");
-  };
+  const titles = ["React", "Native", "For", "The", "First", "Time"];
   return (
     <CenterView>
-      <Text>ハローワールド</Text>
-      <StyleBtn onClick={onClickButton}>
-        <ButtonText>{title}</ButtonText>
-      </StyleBtn>
+      {titles.map((title) => (
+        <Card title={title} />
+      ))}
     </CenterView>
   );
 };
@@ -22,28 +36,25 @@ const center = css`
   justify-content: center;
   align-items: center;
 `;
-const CenterView = styled.View`
-  flex: 1;
-  ${center};
-  background-color: #f5fcff;
-`;
-const StyleBtn = styled.TouchableOpacity`
-  flex-direction: row;
-  ${center};
-  margin-top: 15;
-  border-radius: 20;
-  background-color: #e8483b;
-  width: 150;
-  height: 50;
+const CenterView = styled.ScrollView`
+  background-color: #282c34;
 `;
 const Text = styled.Text`
-  color: black;
-  font-weight: bold;
-`;
-
-const ButtonText = styled.Text`
   color: white;
   font-weight: bold;
+`;
+const CardContainer = styled.View`
+  margin: 24px;
+  width: 240px;
+  height: 150px;
+  background-color: #0e121a;
+  ${center};
+`;
+
+const RoundButtonWrapper = styled.View`
+  background-color: dodgerblue;
+  padding: 3px 12px 3px;
+  border-radius: 10px;
 `;
 
 export default App;
